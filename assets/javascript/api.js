04d0a0364b6fac28b4c93c05ddeed4b183f01d7b
Becca Harris <beccalynharris@gmail.com>
2018-03-12 18:33:14 -0600

    create functions for the buttons and register click of dynamically created buttons

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
+  
   var animals = [];
+  var newButton = $('<button>');
 
-  $('#submit-button').on('click', function() {
+  $('#submit-button').on('click', function(event) {
+    event.preventDefault();
     var userSearch = $('#search-input').val().trim();
-    animals.push(userSearch)
-    var newButton = $('<button>');
-    newButton.attr('data-name', userSearch);
-    newButton.addClass('animal-button');
-    newButton.text(userSearch);
-    $('#button-div').append(newButton)
+    animals.push(userSearch);
+    createButtons();
   })
 
-
-
+  newButton.on('click', function(event) {
+    var animal = $(this).attr('data-name');
+    console.log(animal)
+  });
+  function createButtons() {
+    $('#button-div').empty();
+    for (var i = 0; i < animals.length; i++) {
+      
+      newButton.addClass('animal-button');
+      newButton.attr('data-name', animals[i]);
+      newButton.text(animals[i]);
+      $('#button-div').append(newButton)
+  }
+  console.log(animals)
+}
 })
