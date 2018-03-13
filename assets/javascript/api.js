d40af760e619a58ef2c57f8febc2e0f073f6cb06
Becca Harris <beccalynharris@gmail.com>
2018-03-12 18:11:38 -0600

    get buttons to generate dynamically

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+  var animals = [];
+
+  $('#submit-button').on('click', function() {
+    var userSearch = $('#search-input').val().trim();
+    animals.push(userSearch)
+    var newButton = $('<button>');
+    newButton.attr('data-name', userSearch);
+    newButton.addClass('animal-button');
+    newButton.text(userSearch);
+    $('#button-div').append(newButton)
+  })
+
+
 
 })
