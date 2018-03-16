7ba82e3a1b8ab14787c2bd63d268369b3f5fff1b
Becca Harris <beccalynharris@gmail.com>
2018-03-15 23:13:00 -0600

    get gifs pausing/animating on click

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -16,11 +16,14 @@ $(document).ready(function() {
         animalDiv.addClass('hold-my-gif')
         var gifRating = $('<p>').text('Rating: ' + resultingGifs[i].rating)
         animalDiv.append(gifRating)
-        var resultAnimalGifStill = resultingGifs[i].images.fixed_height_small_still.url;
-        var resultAnimalGifAnimate = resultingGifs[i].images.fixed_height_small.url;
+        var animalGifStill = resultingGifs[i].images.fixed_height_small_still.url;
+        var animalGifAnimate = resultingGifs[i].images.fixed_height_small.url;
         var showGif = $('<img>');
         showGif.addClass('actual-gif');
-        showGif.attr('src', resultAnimalGif);
+        showGif.attr('src', animalGifStill);
+        showGif.attr('data-still', animalGifStill);
+        showGif.attr('data-animate', animalGifAnimate);
+        showGif.attr('data-state', 'still');
         animalDiv.prepend(showGif);
         $('#gifs-div').prepend(animalDiv)
       }
@@ -34,14 +37,11 @@ $(document).ready(function() {
     createButtons();
   })
 
-  $('.actual-gif').on('click', function(event) {
-    console.log("wooooo")
-  })
-
   function createButtons() {
     $('#button-div').empty();
     for (var i = 0; i < animals.length; i++) {
-      var newButton = $('<button type="button" class="btn btn-dark">');
+      var newButton = $('<button>');
+      newButton.addClass('btn btn-dark')
       newButton.addClass('animal-button');
       newButton.attr('data-name', animals[i]);
       newButton.text(animals[i]);
@@ -50,5 +50,15 @@ $(document).ready(function() {
 }
 
 $(document).on("click", ".animal-button", displayAnimalGifs);
+$(document).on("click", ".actual-gif", function() {
+  var state = $(this).attr('data-state');
+  if (state === 'still') {
+    $(this).attr('src', $(this).attr('data-animate'));
+    $(this).attr('data-state', 'animate')
+  } else {
+    $(this).attr('src', $(this).attr('data-still'));
+    $(this).attr('data-state', 'still');
+  }
+});
 
 })
