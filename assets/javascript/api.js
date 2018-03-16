641565c8a0dbc612e826a8fd3e2fb7e408c9c21f
Becca Harris <beccalynharris@gmail.com>
2018-03-15 22:22:00 -0600

    pull still gif instaed of animated

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  var animals = [];
+  var animals = ["Cat","Dog","Gorilla","Pangolin","Penguin"];
 
   function displayAnimalGifs() {
     var animal = $(this).attr('data-name');
-    var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&limit=10&rating=g&api_key=1Pcm3RD1MLLpLwru1GsRqd34UJh96MMs"
+    var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&limit=10&rating=pg&api_key=1Pcm3RD1MLLpLwru1GsRqd34UJh96MMs"
 
     $.ajax({
       url: giphyURL,
@@ -12,21 +12,20 @@ $(document).ready(function() {
       var resultingGifs = response.data;
       console.log(resultingGifs);
       for (var i = 0; i < resultingGifs.length; i++) {
-        var animalDiv = $('<div class="animal">');
+        var animalDiv = $('<div>');
         animalDiv.addClass('hold-my-gif')
-        var resultAnimalGif = resultingGifs[i].images.fixed_height.url;
+        var gifRating = $('<p>').text('Rating: ' + resultingGifs[i].rating)
+        animalDiv.append(gifRating)
+        var resultAnimalGifStill = resultingGifs[i].images.fixed_height_small_still.url;
+        var resultAnimalGifAnimate = resultingGifs[i].images.fixed_height_small.url;
         var showGif = $('<img>');
-        showGif.attr('src', resultAnimalGif)
+        showGif.addClass('actual-gif');
+        showGif.attr('src', resultAnimalGif);
         animalDiv.prepend(showGif);
         $('#gifs-div').prepend(animalDiv)
       }
     })
   }
-
-  
-
-  
-
 
   $('#submit-button').on('click', function(event) {
     event.preventDefault();
@@ -35,10 +34,14 @@ $(document).ready(function() {
     createButtons();
   })
 
+  $('.actual-gif').on('click', function(event) {
+    console.log("wooooo")
+  })
+
   function createButtons() {
     $('#button-div').empty();
     for (var i = 0; i < animals.length; i++) {
-      var newButton = $('<button>');
+      var newButton = $('<button type="button" class="btn btn-dark">');
       newButton.addClass('animal-button');
       newButton.attr('data-name', animals[i]);
       newButton.text(animals[i]);
